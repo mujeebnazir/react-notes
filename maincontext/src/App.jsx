c6ec55d9814aeab1ea97c39453ba496dc1c9e644
mujeebnazir <95225496+mujeebnazir@users.noreply.github.com>
2023-09-18 21:53:22 +0530

    toggleSwitch using context api

---
+++ maincontext/src/App.jsx
@@ -0,0 +1,41 @@
+import "./App.css";
+import { useState, useEffect } from "react";
+import { ThemeProvider } from "./contexts/theme";
+import Themebtn from "./components/Themebtn";
+import Card from "./components/Card";
+
+function App() {
+  const [themeMode, setThemeMode] = useState("light");
+
+  const lightTheme = () => {
+    setThemeMode("light");
+  };
+  const darkTheme = () => {
+    setThemeMode("dark");
+  };
+
+  //actual change in theme
+
+  useEffect(() => {
+    document.querySelector("html").classList.remove("light", "dark");
+    document.querySelector("html").classList.add(themeMode);
+  }, [themeMode]);
+
+  return (
+    <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <Themebtn />
+          </div>
+
+          <div className="w-full max-w-sm mx-auto">
+            <Card />
+          </div>
+        </div>
+      </div>
+    </ThemeProvider>
+  );
+}
+
+export default App;
